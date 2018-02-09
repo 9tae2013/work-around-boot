96eedaac76fc2157cfd8cb7f8b699d038c3705b1
piya_l <piya.tae@gmail.com>
2018-02-09 12:31:00 +0700

    Add logging

--- src/main/java/it/me/tae/workaroundboot/HelloController.java
+++ src/main/java/it/me/tae/workaroundboot/HelloController.java
@@ -1,12 +1,17 @@
 package it.me.tae.workaroundboot;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+    private Logger logger = LoggerFactory.getLogger(HelloController.class);
+
     @GetMapping(value = {"/hello"})
     public String sayHello() {
+        logger.info("Invoke sayHello()");
         return "Hello!!!";
     }
 }
